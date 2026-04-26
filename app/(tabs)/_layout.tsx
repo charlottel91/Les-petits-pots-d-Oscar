32474765238e99cf36f96ecddefd0ef4696c192c
charlottel91 <charlotte.leconte91@gmail.com>
2026-04-26 09:45:27 +0200

    add  icons on tab bar, add list off vegetables, proteines and fruits

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,43 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function AppLayout() {
   return (
     <Tabs
       screenOptions={() => ({
-        tabBarActiveTintColor: 'blue',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: '#910791',
+        tabBarInactiveTintColor: 'grey',
         // headerShown: false,
         animation: 'fade',
       })}
     >
-      <Tabs.Screen name='home' options={{ title: 'Accueil' }} />
-      <Tabs.Screen name='profile' options={{ title: 'Mon compte' }} />
+      <Tabs.Screen
+        name='home'
+        options={{
+          title: '',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='home' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='calendar'
+        options={{
+          title: '',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='calendar' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: '',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='person' size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/calendar/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function Calendar() {
+  return (
+    <View>
+      <Text>Calendar</Text>
+    </View>
+  );
+}
